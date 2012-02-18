bb57d6f7c50a9beb727f25480637ef44939331fe
high <high828@gmail.com>
2012-02-18 02:31:44 -0500

    Added SwfDotNet.
    Changed SwfDotNet to read swf 9 files and added the SymbolClass tag.
    Fixed SwfDotNet swapping the alpha/red channels.
    SwfExport now takes a swf file parameter and reading the images and assets dumping the images with names based on the relative asset.
    Removed NotMissing (unused)

---
+++ SwfDotNet/SwfDotNet.IO/Tags/MetadataTag.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+using SwfDotNet.IO.Utils;
+
+namespace SwfDotNet.IO.Tags
+{
+	public class MetadataTag : BaseTag
+	{
+
+		#region Ctor
+
+		/// <summary>
+		/// Creates a new <see cref="JpegTableTag"/> instance.
+		/// </summary>
+		public MetadataTag()
+		{
+			this._tagCode = (int)TagCodeEnum.Metadata;
+		}
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// JPEG Data is an array of bytes containing the 
+		/// encoding table data.
+		/// </summary>
+		public string Meta { get; set; }
+
+		#endregion
+
+		#region Methods
+
+		/// <summary>
+		/// see <see cref="SwfDotNet.IO.Tags.BaseTag">base class</see>
+		/// </summary>
+		public override void ReadData(byte version, BufferedBinaryReader binaryReader)
+		{
+			RecordHeader rh = new RecordHeader();
+			rh.ReadData(binaryReader);
+
+			int tl = System.Convert.ToInt32(rh.TagLength);
+			Meta = binaryReader.ReadString();
+		}
+
+		/// <summary>
+		/// Gets the size of.
+		/// </summary>
+		/// <returns>Size of this object.</returns>
+		protected int GetSizeOf()
+		{
+			int res = 0;
+			if (Meta != null)
+				res += Meta.Length;
+			return res;
+		}
+
+		/// <summary>
+		/// see <see cref="SwfDotNet.IO.Tags.BaseTag">base class</see>
+		/// </summary>
+		public override void UpdateData(byte version)
+		{
+			MemoryStream m = new MemoryStream();
+			BufferedBinaryWriter w = new BufferedBinaryWriter(m);
+
+			RecordHeader rh = new RecordHeader(TagCode, GetSizeOf());
+
+			rh.WriteTo(w);
+			if (Meta != null)
+				w.Write(Meta);
+
+			w.Flush();
+			// write to data array
+			_data = m.ToArray();
+		}
+
+		/// <summary>
+		/// Serializes the specified writer.
+		/// </summary>
+		/// <param name="writer">Writer.</param>
+		public override void Serialize(XmlWriter writer)
+		{
+			writer.WriteStartElement("Metadata");
+			if (this.Meta != null)
+				writer.WriteAttributeString("MetadataLength", this.Meta.Length.ToString());
+			writer.WriteEndElement();
+		}
+
+		#endregion
+	}
+}
--- SwfExport/Program.cs
+++ SwfExport/Program.cs
@@ -1,14 +1,124 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
+using SwfDotNet.IO.Tags;
+using SwfDotNet.IO.Tags.Types;
 
 namespace SwfExport
 {
 	class Program
 	{
+		#region decoders
+		static Dictionary<Type, Func<object, Tuple<int, Image>>> Decoders = new Dictionary<Type, Func<object, Tuple<int, Image>>>
+ 		{	
+			{typeof(DefineBitsLossLessTag), o => DecodeLossLess((DefineBitsLossLessTag)o)},
+			{typeof(DefineBitsLossLess2Tag), o =>  DecodeLossLess2((DefineBitsLossLess2Tag)o)},
+			{typeof(DefineBitsJpeg2Tag), o =>  DecodeJpeg2((DefineBitsJpeg2Tag)o)},
+			{typeof(DefineBitsJpeg3Tag), o => DecodeJpeg3((DefineBitsJpeg3Tag)o)},
+		};
+		static Tuple<int, Image> DecodeLossLess(DefineBitsLossLessTag tag)
+		{
+			return Tuple.Create((int)tag.CharacterId, tag.DecompileToImage());
+		}
+		static Tuple<int, Image> DecodeLossLess2(DefineBitsLossLess2Tag tag)
+		{
+			return Tuple.Create((int)tag.CharacterId, tag.DecompileToImage());
+		}
+		static Tuple<int, Image> DecodeJpeg2(DefineBitsJpeg2Tag tag)
+		{
+			return Tuple.Create((int)tag.CharacterId, tag.DecompileToImage());
+		}
+		static Tuple<int, Image> DecodeJpeg3(DefineBitsJpeg3Tag tag)
+		{
+			return Tuple.Create((int)tag.CharacterId, tag.DecompileToImage());
+		}
+		#endregion
 		static void Main(string[] args)
 		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("Usage: SwfExport <file.swf>");
+				return;
+			}
+			var file = args[0];
+			if (!File.Exists(file))
+			{
+				Console.WriteLine("File not found");
+				return;
+			}
+			try
+			{
+
+				Console.WriteLine("Loading swf");
+
+				var sr = new SwfDotNet.IO.SwfReader(File.OpenRead(file));
+				var swf = sr.ReadSwf();
+				var tags = swf.Tags.Cast<BaseTag>().ToList();
+
+				Console.WriteLine("Gettings assets");
+
+				var asserts = new List<Assert>();
+				asserts.AddRange(GetSymbolClasses(tags));
+				asserts.AddRange(GetExportAsserts(tags));
+
+				Console.WriteLine("Loading images");
+
+				var imgs = GetImages(tags);
+
+				Console.WriteLine("Dumping images");
+
+				if (!Directory.Exists("output"))
+					Directory.CreateDirectory("output");
+				if (!Directory.Exists("output\\unknown"))
+					Directory.CreateDirectory("output\\unknown");
+
+				var sw = Stopwatch.StartNew();
+				int i = 0;
+				foreach (var img in imgs)
+				{
+					var asset = asserts.Find(a => a.TargetCharacterId == img.Item1);
+					var name = (asset != null ? asset.Name : "unknown\\" + img.Item1.ToString(CultureInfo.InvariantCulture)) + ".png";
+					img.Item2.Save("output\\" + name, ImageFormat.Png);
+					i++;
+					if (sw.ElapsedMilliseconds > 2000)
+					{
+						Console.WriteLine("Progress {0}/{1}", i, imgs.Count);
+						sw.Restart();
+					}
+				}
+
+
+				Console.WriteLine("Success");
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Error: " + ex);
+			}
+		}
+
+		static List<Tuple<int, Image>> GetImages(IEnumerable<BaseTag> tags)
+		{
+			var imgs = tags.Where(t => Decoders.ContainsKey(t.GetType()));
+			return imgs.Select(t => Decoders[t.GetType()](t)).ToList();
+		}
+
+		static IEnumerable<Assert> GetSymbolClasses(IEnumerable<BaseTag> tags)
+		{
+			var syms = tags.Where(t => t is SymbolClassTag).Cast<SymbolClassTag>();
+			return syms.Select(s => s.ExportedCharacters).SelectMany(s => s.Cast<Assert>().ToList());
+		}
+		static IEnumerable<Assert> GetExportAsserts(IEnumerable<BaseTag> tags)
+		{
+			var syms = tags.Where(t => t is ExportAssetsTag).Cast<ExportAssetsTag>();
+			return syms.Select(s => s.ExportedCharacters).SelectMany(s => s.Cast<Assert>().ToList());
 		}
 	}
 }
